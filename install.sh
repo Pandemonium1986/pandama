c8a545ea23e1278b7c73c68c5cebca122ad6c85e
Pandemonium1986 <michael.maffait@gmail.com>
2018-10-16 21:49:31 +0200

    add : provision docker and backports

--- install.sh
+++ install.sh
@@ -1,14 +1,65 @@
 #!/usr/bin/env bash
+#############################################
+##   Pandama : Vagrant Shell provisioner   ##
+#############################################
 
+#####################
+# Apt configuration #
+#####################
+
+#-- Install mandatory packages
 apt-get update
-apt-get install -y build-essential cowsay curl dkms dnsutils fonts-powerline git gitk htop libfortune-perl man mlocate module-assistant net-tools nmap powerline sudo tmux tree unzip vim zsh
+apt-get install -y \
+	build-essential \
+	curl \
+	cowsay \
+	dkms \
+	dnsutils \
+	fonts-powerline \
+	htop \
+	libfortune-perl \
+	man \
+	mlocate \
+	module-assistant\
+	net-tools \
+	nmap \
+	powerline \
+	sudo \
+	tree \
+	unzip \
+	vim
+
+#-- Docker
+apt-get install -y \
+	apt-transport-https \
+	ca-certificates \
+	gnupg2 \
+	software-properties-common
+
+#-- Configure external source list.
+echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+
+#-- Backpos
+apt-get -t stretch-backports install -y \
+	ansible \
+	curl \
+	git \
+	gitk \
+	tmux \
+	zsh
+
+#-- Docker
+apt-get install -y docker-ce
+ # ---------------------------------------------------------------------------- #
+
 if ! [ -L /home/vagrant/git/Pandemonium1986 ]; then
 	mkdir -p /home/vagrant/git/Pandemonium1986
 	cd  /home/vagrant/git/Pandemonium1986
 	git clone https://github.com/Pandemonium1986/dotfiles
-fi
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
 fi
